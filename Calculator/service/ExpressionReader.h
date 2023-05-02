32cd9e5679515d420b56efdaea3c9cb3c5fcfeae
minakdanCVUT <132306332+minakdanCVUT@users.noreply.github.com>
2023-05-02 19:56:55 +0200

    class ExpressionReader : made a header file

---
+++ Calculator/service/ExpressionReader.h
@@ -0,0 +1,13 @@
+#ifndef SEMESTRALKA_EXPRESSIONREADER_H
+#define SEMESTRALKA_EXPRESSIONREADER_H
+
+
+class ExpressionReader {
+
+public:
+    void setString(std::string basicString);
+};
+
+
+#endif //SEMESTRALKA_EXPRESSIONREADER_H
+
