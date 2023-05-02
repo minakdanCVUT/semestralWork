dfb4bb806ccce4a8a93436c1eb7229e6597fdea4
minakdanCVUT <132306332+minakdanCVUT@users.noreply.github.com>
2023-05-02 19:57:36 +0200

    class SystemReader : made a header file

---
+++ Calculator/service/SystemReader.h
@@ -0,0 +1,13 @@
+#ifndef SEMESTRALKA_SYSTEMREADER_H
+#define SEMESTRALKA_SYSTEMREADER_H
+
+
+class SystemReader {
+
+public:
+    void read(std::string basicString);
+};
+
+
+#endif //SEMESTRALKA_SYSTEMREADER_H
+
